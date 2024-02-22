2925927fad8a919bfad568484f1f01dc15d4ac21
boya srikanth <boyasrikanth517@gmail.com>
2024-02-22 19:35:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React , { useState } from "react";
+
+const Door=()=>{
+
+
+    useState()
+
+    return(
+        <section className="continer">
+            <h1 className="name">hello srikanth</h1>
+        </section>
+    )
+}
+
+export default Door;
